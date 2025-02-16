cba03f0ee807132e8675a4458596f2bbdbbbe5cc
Yomi <123415710+Yommi@users.noreply.github.com>
2025-02-16 13:58:43 -0500

    small fix

--- controllers/factoryController.js
+++ controllers/factoryController.js
@@ -63,6 +63,7 @@ exports.updateOne = (model) => {
     if (model === User) {
       req.body = filterObj(
         req.body,
+        'userName',
         'role',
         'password',
         'passwordConfirm'
