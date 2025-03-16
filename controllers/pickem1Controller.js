b5651047eabec69b9c0073a4ad2ab78a9dd1b8de
Yomi <123415710+Yommi@users.noreply.github.com>
2025-03-15 21:59:28 -0400

    post multiple picks at once

--- controllers/pickem1Controller.js
+++ controllers/pickem1Controller.js
@@ -7,15 +7,20 @@ exports.getAllPicks = factoryController.getAll(Pickem1);
 exports.getPick = factoryController.getOne(Pickem1);
 exports.createPick = catchAsync(async (req, res, next) => {
   if (!Array.isArray(req.body)) {
-    return next(new AppError(404, 'request has to be an array'));
+    return next(new AppError(400, 'Request has to be an array'));
   }
 
   const picks = req.body;
 
-  picks.map(async (i, pick) => {
-    await Pickem1.create(pick);
-  });
+  const resultPicks = await Promise.all(
+    picks.map(async (pick) => {
+      return Pickem1.create(pick);
+    })
+  );
+
   res.status(200).json({
     status: 'success',
+    results: resultPicks.length,
+    data: resultPicks,
   });
 });
