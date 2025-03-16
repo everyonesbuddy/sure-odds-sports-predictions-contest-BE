ad2957e7635f3aae36a7e90aa3e55995768d0dc8
Yomi <123415710+Yommi@users.noreply.github.com>
2025-03-15 21:48:02 -0400

    .

--- controllers/pickem1Controller.js
+++ controllers/pickem1Controller.js
@@ -1,6 +1,21 @@
 const Pickem1 = require('../models/pickModel');
 const factoryController = require('./factoryController');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
-exports.createPick = factoryController.createOne(Pickem1);
 exports.getAllPicks = factoryController.getAll(Pickem1);
 exports.getPick = factoryController.getOne(Pickem1);
+exports.createPick = catchAsync(async (req, res, next) => {
+  if (!Array.isArray(req.body)) {
+    return next(new AppError(404, 'request has to be an array'));
+  }
+
+  const picks = req.body;
+
+  picks.map(async (i, pick) => {
+    await Pickem1.create(pick);
+  });
+  res.status(200).json({
+    status: 'success',
+  });
+});
