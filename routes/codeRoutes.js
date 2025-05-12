f9dd09e03289afdbc9340b7d0b1d520587864da8
Yomi <123415710+Yommi@users.noreply.github.com>
2025-05-12 00:23:54 -0400

    small changes

--- routes/codeRoutes.js
+++ routes/codeRoutes.js
@@ -4,8 +4,6 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.use(authController.protect);
-
 router
   .route('/')
   .get(codeController.getAllCodes)
